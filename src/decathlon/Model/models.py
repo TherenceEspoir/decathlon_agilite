a4fd23442b323d7fd0453d51e49fa48f1894c60a
Cgoudal <goudal.clement1@gmail.com>
2024-01-18 10:44:46 +0100

    ajout / modification class "models"

--- src/decathlon/Model/models.py
+++ src/decathlon/Model/models.py
@@ -7,15 +7,43 @@ import uuid
 class Identifiable(BaseModel):
     uuid: typing.Union[uuid.UUID, None]
 
+
+class user(Identifiable):
+    id: int
+    name: str
+    mail: str
+    password: str
+    bearth_date: datetime
+    
+    
+    
 #classe unité avec abréviation de l'unité et identifiant
 class Unite(Identifiable):
+    id: int
     name: str
     abbreviation: str
 
 #Classe healthData avec les différents attributs poids , frequence cardiaque, durée de sommeil, nombre de pas 
     
 class HealthData(Identifiable):
-    poids: int
-    frequence_cardiaque: int
-    duree_sommeil: int
+    id: int
+    id_user: int
+    date: datetime
     nombre_pas: int
+    duree_sommeil: int
+    u_duree_sommeil: int
+    frequence_cardiaque: int
+    u_frequence_cardiaque: int
+    poids: int
+    u_poids: int
+    taille: float
+    u_taille: int
+    
+    
+
+class Unite_Convertion(Identifiable):
+    from_id: int
+    to_id: int
+    rapport: float
+
+
