230c153d27f11318f9191c84fa954a7b7a55ac87
valette.adrien <adrien.valetteetude@gmail.com>
2024-01-18 16:04:23 +0100

    BD est désormais un singleton, suppression de la méthode connection

--- src/decathlon/Controller/health.py
+++ src/decathlon/Controller/health.py
@@ -2,21 +2,22 @@ from http.client import HTTPException
 from typing import Union
 import uuid
 
-from src.decathlon.connexion import get_connection
+from src.decathlon.Controller.init_bdd import DecathlonDBManager
 
 from src.decathlon.Model.health import HealthData
 
 from datetime import datetime
 
-db_connection=get_connection()
+
+db_connection=DecathlonDBManager().__enter__()
 
 
 
 #fonction qui permet de créer un healthData
 # fonction qui permet de créer un healthData
 def create_healthData(healthData: HealthData):
-    connection = get_connection()
-    connection.execute(
+    
+    db_connection.execute(
         "INSERT INTO healthData (id, id_user, date, nombre_pas, duree_sommeil, u_duree_sommeil, frequence_cardiaque, u_frequence_cardiaque, poids, u_poids, taille, u_taille) VALUES(?,?,?,?,?,?,?,?,?,?,?,?);",
         (
             str(healthData.uuid) if healthData.uuid else None,
@@ -33,39 +34,38 @@ def create_healthData(healthData: HealthData):
             healthData.u_taille,
         ),
     )
-    connection.commit()
-    connection.close()
+    db_connection.commit()
+    db_connection.close()
 
     return {"Donnée de santé bien ajoutée"}
 
 
 #fonction qui permet de lister les healthData
 def healthData(id_user: int):
-    with get_connection() as connection:
-        cursor = connection.execute("SELECT * FROM healthData Where healthData.id_user == ?", (id_user,))
-        healthData = cursor.fetchall()
+    cursor = db_connection.execute("SELECT * FROM healthData Where healthData.id_user == ?", (id_user,))
+    healthData = cursor.fetchall()
 
-        print(healthData)
-        result = []
+    print(healthData)
+    result = []
 
-        for row in healthData:
-            try:
-                result.append(HealthData(
-                    uuid=row[0],
-                    id_user=row[1],
-                    date=row[2] if row[2] else datetime.now(),
-                    nombre_pas=row[3],
-                    duree_sommeil=row[4],
-                    u_duree_sommeil=row[5],
-                    frequence_cardiaque=row[6],
-                    u_frequence_cardiaque=row[7],
-                    poids=row[8],
-                    u_poids=row[9],
-                    taille=row[10],
-                    u_taille=row[11],
-                ))
-            except Exception as e:
-                print(f"Error processing row {row}: {e}")
+    for row in healthData:
+        try:
+            result.append(HealthData(
+                uuid=row[0],
+                id_user=row[1],
+                date=row[2] if row[2] else datetime.now(),
+                nombre_pas=row[3],
+                duree_sommeil=row[4],
+                u_duree_sommeil=row[5],
+                frequence_cardiaque=row[6],
+                u_frequence_cardiaque=row[7],
+                poids=row[8],
+                u_poids=row[9],
+                taille=row[10],
+                u_taille=row[11],
+            ))
+        except Exception as e:
+            print(f"Error processing row {row}: {e}")
 
     return {"health data: {}".format(result)}
 
--- src/decathlon/Controller/init_bdd.py
+++ src/decathlon/Controller/init_bdd.py
@@ -1,11 +1,25 @@
 import os
-from src.decathlon.Model.health import HealthData
-from src.decathlon.connexion import get_connection
+import sqlite3
 
 class DecathlonDBManager:
-    def __init__(self, db_filename="decathlon.db"):
-        self.db_filename = db_filename
-        self.db_connection = get_connection()
+    _instance = None
+
+    def __new__(cls, db_filename="decathlon.db"):
+        if cls._instance is None:
+            cls._instance = super(DecathlonDBManager, cls).__new__(cls)
+            cls._instance.db_filename = db_filename
+            cls._instance.db_connection = None
+        return cls._instance
+
+    def __enter__(self):
+        if self.db_connection is None:
+            self.db_connection = sqlite3.connect(self.db_filename)
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self.db_connection is not None:
+            self.db_connection.close()
+            self.db_connection = None
 
     def check_and_create_db(self):
         # Vérifier si la base de données existe déjà
@@ -13,53 +27,51 @@ class DecathlonDBManager:
 
         # Si la base de données n'existe pas, créez-la et les tables
         if not db_exists:
-            self.db_connection.execute(
-                """
-                CREATE TABLE healthData (
-                    id TEXT PRIMARY KEY,
-                    id_user INTEGER,
-                    date DATETIME, 
-                    nombre_pas INTEGER,
-                    duree_sommeil INTEGER,
-                    u_duree_sommeil INTEGER,
-                    frequence_cardiaque INTEGER,
-                    u_frequence_cardiaque INTEGER,
-                    poids INTEGER,
-                    u_poids INTEGER,
-                    taille FLOAT,
-                    u_taille INTEGER
-                );"""
-            )
-            self.db_connection.execute(
-                """
-                CREATE TABLE user (
-                    id TEXT PRIMARY KEY,
-                    name TEXT,
-                    mail TEXT,
-                    password TEXT,
-                    birth_date DATETIME
-                );"""
-            )
-            self.db_connection.execute(
-                """
-                CREATE TABLE unite (
-                    id TEXT PRIMARY KEY,
-                    name TEXT,
-                    abbreviation TEXT
-                );"""
-            )
-            self.db_connection.execute(
-                """
-                CREATE TABLE unite_convertion (
-                    from_id TEXT,
-                    to_id TEXT,
-                    rapport FLOAT
-                );"""
-            )
-            self.db_connection.commit()
-
-        # Fermez la connexion à la base de données
-        self.db_connection.close()
+            with self:
+                self.db_connection.execute(
+                    """
+                    CREATE TABLE healthData (
+                        id TEXT PRIMARY KEY,
+                        id_user INTEGER,
+                        date DATETIME, 
+                        nombre_pas INTEGER,
+                        duree_sommeil INTEGER,
+                        u_duree_sommeil INTEGER,
+                        frequence_cardiaque INTEGER,
+                        u_frequence_cardiaque INTEGER,
+                        poids INTEGER,
+                        u_poids INTEGER,
+                        taille FLOAT,
+                        u_taille INTEGER
+                    );"""
+                )
+                self.db_connection.execute(
+                    """
+                    CREATE TABLE user (
+                        id TEXT PRIMARY KEY,
+                        name TEXT,
+                        mail TEXT,
+                        password TEXT,
+                        birth_date DATETIME
+                    );"""
+                )
+                self.db_connection.execute(
+                    """
+                    CREATE TABLE unite (
+                        id TEXT PRIMARY KEY,
+                        name TEXT,
+                        abbreviation TEXT
+                    );"""
+                )
+                self.db_connection.execute(
+                    """
+                    CREATE TABLE unite_convertion (
+                        from_id TEXT,
+                        to_id TEXT,
+                        rapport FLOAT
+                    );"""
+                )
+                self.db_connection.commit()
 
         # Vérifiez à nouveau si la base de données existe après la fermeture
         db_exists = os.path.exists(self.db_filename)
@@ -69,6 +81,8 @@ class DecathlonDBManager:
         else:
             print("La base de données a été créée.")
 
-# Utilisation de la classe
-db_manager = DecathlonDBManager()
-db_manager.check_and_create_db()
+# Utilisation de la classe avec la gestion de contexte
+with DecathlonDBManager() as db_manager:
+    db_manager.check_and_create_db()
+
+
