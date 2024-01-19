c92818149d76a1ab71482f2b4f170598280fcabe
valette.adrien <adrien.valetteetude@gmail.com>
2024-01-19 10:22:47 +0100

    Fin adaptation du test unitaire

--- test/test_BD.py
+++ test/test_BD.py
@@ -1,18 +1,17 @@
-from src.decathlon.Controller.init_bdd import DecathlonDBManager
+import src.decathlon.Controller.init_bdd 
+import sqlite3
 
 class TestBD:
     def test_BD(self):
-        with DecathlonDBManager() as db_manager:
-            db_manager.check_and_create_db()
-            connexion = db_manager.db_connection
-            assert connexion is not None, "La connexion à la base de données ne doit pas être nulle"
+        
+        connexion = sqlite3.connect("mydatabase.db")
+        assert connexion is not None, "La connexion à la base de données ne doit pas être nulle"
 
-            cursor = connexion.execute("SELECT name FROM sqlite_master WHERE type='table';")
-            tables = cursor.fetchall()
-            assert len(tables) == 4, "Il devrait y avoir 4 tables dans la base de données"
+        cursor = connexion.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        tables = cursor.fetchall()
+        assert len(tables) == 4, "Il devrait y avoir 4 tables dans la base de données"
 
-            table_names = [table[0] for table in tables]
-            assert 'healthData' in table_names, "La table 'healthData' devrait exister"
-            assert 'user' in table_names, "La table 'user' devrait exister"
-            assert 'unite' in table_names, "La table 'unite' devrait exister"
-            assert 'unite_convertion' in table_names, "La table 'unite_convertion' devrait exister"
+        table_names = [table[0] for table in tables]
+        assert 'health_data' in table_names, "La table 'health_data' devrait exister"
+        assert 'users' in table_names, "La table 'users' devrait exister"
+        assert 'unites' in table_names, "La table 'unites' devrait exister"
