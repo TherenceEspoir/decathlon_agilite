fa05acc9557afb1307cefc404a98d94301def185
Cgoudal <goudal.clement1@gmail.com>
2024-01-19 10:32:54 +0100

    modif user endpoint

--- src/decathlon/Controller/user.py
+++ src/decathlon/Controller/user.py
@@ -1,7 +1,7 @@
 
 from fastapi import Depends, FastAPI, Header, HTTPException
 from src.decathlon.Controller.init_bdd import conn, cursor
-from src.decathlon.Model.user import User
+from src.decathlon.Model.user import User, UserInput
 # from src.decathlon.main import app
 
 from fastapi.middleware.cors import CORSMiddleware
@@ -33,34 +33,21 @@ def get_user_by_id(user_id: int):
 
     return user
 
-# @app.get("/users/{user_id}")
-# async def read_user(user_id: int):
-#     cursor.execute("SELECT * FROM users WHERE id=?", (user_id,))
-#     user_data = cursor.fetchone()
-
-#     if user_data is None:
-#         raise HTTPException(status_code=404, detail="User not found")
-
-#     user = User(
-#         id=user_data[0],
-#         name=user_data[1],
-#         mail=user_data[2],
-#         password=user_data[3],
-#         birth_date=user_data[4]
-#     )
-
-#     return user
-
-
-def post_user(user:User):
+def post_user(userInput:UserInput):
     try:
         cursor.execute("INSERT INTO users (name, mail, password, birth_date) VALUES (?, ?, ?, ?)",
-                       (user.name, user.mail, user.password, user.birth_date))
+                       (userInput.name, userInput.mail, userInput.password, userInput.birth_date))
         conn.commit()
 
         user_id = cursor.lastrowid
 
-        user.id = user_id
+        user = User(
+            id=user_id,
+            name=userInput.name,
+            mail=userInput.mail,
+            password=userInput.password,
+            birth_date=userInput.birth_date
+        )
 
         return user
     except Exception as e:
