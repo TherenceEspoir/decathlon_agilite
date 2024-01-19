4993a3e1fac8876127a55a02926016c1251840bb
Cgoudal <goudal.clement1@gmail.com>
2024-01-19 15:38:27 +0100

    update test

--- test/test_BD.py
+++ test/test_BD.py
@@ -9,9 +9,8 @@ class TestBD:
 
         cursor = connexion.execute("SELECT name FROM sqlite_master WHERE type='table';")
         tables = cursor.fetchall()
-        assert len(tables) == 4, "Il devrait y avoir 4 tables dans la base de données"
+        assert len(tables) == 3, "Il devrait y avoir 3 tables dans la base de données"
 
         table_names = [table[0] for table in tables]
         assert 'health_data' in table_names, "La table 'health_data' devrait exister"
         assert 'users' in table_names, "La table 'users' devrait exister"
-        assert 'unites' in table_names, "La table 'unites' devrait exister"
